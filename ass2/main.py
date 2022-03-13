a2df969cf14f1662ca5fe7beb747177cc54b4136
profoundSpreadsheets <paul.langt@gmail.com>
2022-03-14 00:03:28 +0100

    simple db connect

--- ass2/main.py
+++ ass2/main.py
@@ -1,2 +1,37 @@
+from tkinter import E
+import psycopg2
+
+
 def main():
-    print("hello world")
+    conn = None
+
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            database="iop",
+            user="paul",
+            password="password"
+        )
+
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT version()")
+
+        # display the PostgreSQL database server version
+        db_version = cursor.fetchone()
+        print(db_version)
+
+        # close the communication with the PostgreSQL
+        cursor.close()
+
+    except (Exception, psycopg2.DatabaseError) as e:
+        print(e)
+
+    finally:
+        if conn is not None:
+            conn.close()
+            print("Database connect closed.")
+
+
+if __name__ == "__main__":
+    main()
