f130de12d404ed21d102211ccdd27c876abc162f
profoundSpreadsheets <paul.langt@gmail.com>
2022-03-16 23:58:22 +0100

    view to view rows of all user tables

--- ass2/create.sql
+++ ass2/create.sql
@@ -91,3 +91,29 @@ CREATE TABLE TESTPROTOCOL(
     PRIMARY KEY(Registration, ProtocolId),
     FOREIGN KEY (Registration) REFERENCES PLANE (Registration) ON DELETE CASCADE
 );
+
+create or replace function 
+count_rows(schema text, tablename text) returns integer
+as
+$body$
+declare
+  result integer;
+  query varchar;
+begin
+  query := 'SELECT count(1) FROM ' || schema || '.' || tablename;
+  execute query into result;
+  return result;
+end;
+$body$
+language plpgsql;
+
+create or replace view rows_all_tables as
+select 
+  table_schema,
+  table_name, 
+  count_rows(table_schema, table_name)
+from information_schema.tables
+where 
+  table_schema not in ('pg_catalog', 'information_schema') 
+  and table_type='BASE TABLE'
+order by 3 desc;
